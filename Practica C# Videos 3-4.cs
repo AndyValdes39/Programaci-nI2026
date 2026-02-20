8b5f267efa94d8268e765312f437f59cbf94da2e
AndyValdes39 <andydvaldes@gmail.com>
2026-02-20 17:44:59 -0600

    Add files via upload

---
+++ Practica C# Videos 3-4.cs
@@ -0,0 +1,36 @@
+﻿
+
+//Video#3
+
+Console.WriteLine(45);
+Console.WriteLine("Holaaa");
+Console.WriteLine(50 + 60);
+
+
+//Video#4
+
+//int = 7777;
+//Permite almacenar números enteros
+
+//double = 7.77;
+//Permite almacenar numeros con decimales
+
+
+//bool = true / false
+//Permite definir dos estados true y false
+
+
+//char = 'A' (7)
+//Permite definir caracteres de tipo UniCode
+
+
+//float = 1.77f
+//Permite guardar valores decimales de 32bit
+
+
+//string = 'holholhol'
+//Permite manejar una cadena de texto
+
+
+//Otros tipos de datos 
+//object, class, interface
