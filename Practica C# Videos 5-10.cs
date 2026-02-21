cd346d4711dba6c6e959833d131ad499b4b3e2a0
AndyValdes39 <andydvaldes@gmail.com>
2026-02-20 20:38:51 -0600

    Add files via upload

---
+++ Practica C# Videos 5-10.cs
@@ -0,0 +1,101 @@
+﻿
+//Video#5
+//Variable: Contenedor que almacena datos de un tipo en especifico.
+//Utilidad de una variable: Manipular informacion durante la ejecucion de un programa.
+
+//Declarar una variable:
+
+//Las variables pueden declararse dependiendo del tipo de dato con el que se busca trabajar
+
+
+//string mi_nombre = "Andy";
+//int edad = 19;
+//bool activo = false;
+//DateTime fecha = DateTime.Now;
+//float precio = 0;
+//decimal descuento;
+
+//Console.WriteLine(mi_nombre);
+//Console.WriteLine(edad);
+//Console.WriteLine(activo);
+//Console.WriteLine(fecha);
+//Console.WriteLine(precio);
+//Console.ReadKey();
+
+
+//Video#6
+
+//Concatenacion de datos:
+
+
+//string mi_nombre = "Andy";
+//int edad = 19;
+//bool activo = false;
+//DateTime fecha = DateTime.Now;
+//float precio = 0;
+//decimal descuento;
+
+//Console.WriteLine("Nombre de usuario" + mi_nombre);
+//Console.WriteLine("Edad" + edad);
+//Console.WriteLine($"Nombre de usuario: {mi_nombre} {edad}");
+
+
+//Video#7
+//Comentarios de codigo
+/*
+Console.WriteLine(mi_nombre);
+Console.WriteLine(edad);
+Console.WriteLine(activo);
+Console.WriteLine(fecha);
+Console.WriteLine(precio);
+Console.ReadKey();*/
+
+//Añadiendo "/*" al inicio de un parrafo de codigo y "*/" al final de este lo invalida ayudando a no seleccionar linea por linea.
+
+
+
+
+//Video#8
+//Constantes 
+
+//Una cosntante es un valor fijo que no cambia durante la ejecución de un programa.
+/*
+const int numero2 = 150;
+Console.WriteLine("Valor de constante: " + numero2);*/
+
+
+//Video#9
+//Casting: Convertir un tipo de dato a otro
+
+//casting implicito
+/*
+int valor = 200;
+double total = valor;
+Console.WriteLine("implicito: " + total);*/
+
+//casting explicito
+/*
+double precio = 500.23;
+int descuento = (int)precio;
+Console.WriteLine("Explicito: " + descuento);
+
+char letra = 'A';
+int codigoAsci = letra;
+Console.WriteLine(codigoAsci);
+
+string palabra = "123456";
+int numero = Convert.ToInt32(palabra);
+Console.WriteLine(numero);
+*/
+
+
+//Video#10
+//Operadores Artimeticos
+/*
+int numero1 = 10;
+int numero2 = 25;
+int total = 10 + 25;
+Console.WriteLine("Total de la suma: " + total);
+*/
+
+//Mediante este codigo pueden elaborarse distintos tipos de operadores aritmeticos, cambiando el simbolo de la operación
